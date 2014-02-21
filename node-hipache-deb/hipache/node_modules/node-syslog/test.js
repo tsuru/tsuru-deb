e8e13f9e1cb30057c8f9ba44c1183c79367661d2
Paulo Sousa <paulo.sousa@corp.globo.com>
2014-02-21 12:01:35 -0300

    nodejs - fix shebang patch, fix dev package with v8 headers // node-hipache - fix connreset, update upstream http-proxy, removing garbase node_modules dir

--- node-hipache-deb/hipache/node_modules/node-syslog/test.js
+++
@@ -1,8 +0,0 @@
-var Syslog = require('./node-syslog');
-
-Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
-Syslog.log(Syslog.LOG_INFO, "news info log test");
-Syslog.log(Syslog.LOG_ERR, "news log error test");
-Syslog.log(Syslog.LOG_DEBUG, "Last log message as debug: " + new Date());
-Syslog.close();
-
