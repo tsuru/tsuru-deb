6bc4eb52f1185fa088894cc97ee58d231e7d7213
Paulo Sousa <paulo.sousa@corp.globo.com>
2015-08-21 10:56:46 -0300

    tsuru-server: new upstream RC // adding rc version logic

--- variables.mk
+++ variables.mk
@@ -26,7 +26,7 @@ DAILY_BUILD_EXCEPT = dh-golang golang nodejs node-hipache
 
 export CHECK_LAUNCHPAD_FAIL := "no_error"
 
-TAG_tsuru-server = 0.11.3
+TAG_tsuru-server = 0.12-rc1
 TAG_serf = 0.4.1
 TAG_gandalf-server = 0.7.1
 TAG_archive-server = 0.1.2
