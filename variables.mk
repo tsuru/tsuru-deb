6497238dcf08e474f07f6ecfe23010fbd3cc2341
Francisco Souza <fss@corp.globo.com>
2015-09-02 19:43:54 -0300

    tsuru-server: packaging the 0.12.1 release

--- variables.mk
+++ variables.mk
@@ -24,7 +24,7 @@ DAILY_BUILD_EXCEPT = dh-golang golang nodejs node-hipache
 
 export CHECK_LAUNCHPAD_FAIL := "no_error"
 
-TAG_tsuru-server = 0.12.0
+TAG_tsuru-server = 0.12.1
 TAG_serf = 0.4.1
 TAG_gandalf-server = 0.7.1
 TAG_archive-server = 0.1.2
