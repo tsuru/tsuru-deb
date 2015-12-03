907f7194890e2aac3b94836c21103d82029970d9
Francisco Souza <fss@corp.globo.com>
2015-12-03 18:48:03 +0000

    deploy-agent: new release

--- variables.mk
+++ variables.mk
@@ -42,7 +42,7 @@ TAG_dh-golang = 1.5
 TAG_golang = 1.4.0
 TAG_nodejs = 0.10.26.3
 TAG_node-hipache = 0.2.5
-TAG_deploy-agent = 0.2.0
+TAG_deploy-agent = 0.2.1
 TAG_planb = 0.1.3
 
 -include variables.local.mk
